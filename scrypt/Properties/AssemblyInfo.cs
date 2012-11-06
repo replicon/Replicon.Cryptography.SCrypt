9a1780ac7eb1146548d83913e68e9f7c67ff58a8
Mathieu Fenniak <mathieu.fenniak@replicon.com>
2012-11-06 14:39:01 -0700

    Mark as version 1.1.6.1
    Added scrypt.dll wrapper library for mixed-mode assembly to
    automatically select x86 or x64 assembly, extract it to a temp folder,
    and load it into the appdomain

--- scrypt/Properties/AssemblyInfo.cs
+++ scrypt/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("scrypt")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("scrypt key-derivation function wrapper assembly")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Replicon Inc.")]
 [assembly: AssemblyProduct("scrypt")]
-[assembly: AssemblyCopyright("Copyright ©  2012")]
+[assembly: AssemblyCopyright("Copyright Replicon Inc. © 2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.6.1")]
+[assembly: AssemblyFileVersion("1.1.6.1")]
