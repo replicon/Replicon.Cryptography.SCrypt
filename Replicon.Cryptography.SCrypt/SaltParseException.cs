2569e10e258d408b208aa7e0baf2711a3b3171f5
Mathieu Fenniak <mathieu.fenniak@replicon.com>
2012-11-30 11:14:12 -0700

    Move managed code to SCrypt library
    Also, add TryParseSalt and ParseSalt methods to SCrypt.  Increment
    version to 1.1.6.5.

---
+++ Replicon.Cryptography.SCrypt/SaltParseException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Replicon.Cryptography.SCrypt
+{
+    /// <summary>
+    /// Exception thrown when a SCrypt salt string is unparsable.
+    /// </summary>
+    [Serializable]
+    public class SaltParseException : Exception
+    {
+        internal SaltParseException(string message)
+            : base(message)
+        {
+        }
+    }
+}
