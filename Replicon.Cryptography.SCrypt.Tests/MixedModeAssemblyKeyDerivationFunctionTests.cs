8d46ba083191085e6581c5ba6a76ac20d1d9205c
Mathieu Fenniak <biziqe@mathieu.fenniak.net>
2012-12-15 13:43:34 -0700

    Add Replicon.Cryptography.SCrypt.Tests
    Using scrypt test vectors from Colin's original paper

---
+++ Replicon.Cryptography.SCrypt.Tests/MixedModeAssemblyKeyDerivationFunctionTests.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+namespace Replicon.Cryptography.SCrypt.Tests
+{
+    [TestFixture]
+    public class MixedModeAssemblyKeyDerivationFunctionTests : KeyDerivationFunctionTests
+    {
+        protected override IKeyDerivationFunction Create()
+        {
+            return Factory.CreateNativeKeyDerivationFunction();
+        }
+    }
+}
