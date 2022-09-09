6577ca23ca259b2f3a3c48c4aaa1fef222ec7c1f
Dheeraj V <81616103+repl-dheeraj-v@users.noreply.github.com>
2022-09-09 17:10:54 +0530

    re-target to net35

--- Replicon.Cryptography.SCrypt/KeyDerivationFunction.cs
+++ Replicon.Cryptography.SCrypt/KeyDerivationFunction.cs
@@ -1,12 +1,12 @@
-﻿using Bryllite.Cryptography.Scrypt;
-
+﻿
 namespace Replicon.Cryptography.SCrypt
 {
     class KeyDerivationFunction : IKeyDerivationFunction
     {
         public byte[] DeriveKey(byte[] password, byte[] salt, ulong N, uint r, uint p, uint derivedKeyLengthBytes)
         {
-            return Scrypt.CryptoScrypt(password, salt, (int)N, (int)r, (int)p, (int)derivedKeyLengthBytes);
+          // FIXME: Use https://www.nuget.org/packages/Scrypt.NET/ once PR #11 is released in https://github.com/viniciuschiele/Scrypt
+          return ScryptEncoder.CryptoScrypt(password, salt, (int)N, (int)r, (int)p, (int)derivedKeyLengthBytes);
         }
     }
 }
